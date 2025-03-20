89ca7378effba90d7550b6acbaab5fa0668312ef
reuelsias <reuelsias@icloud.com>
2025-03-20 08:58:27 +0000

    Menu created with option to set translation and change reference

--- bible.py
+++ bible.py
@@ -1,29 +1,67 @@
 import requests
-import sys
+import re
 
-arguments = sys.argv
-url_base = "https://bible-api.com/"
+url_api = "https://bible-api.com"
+actual_reference = None
+translation = None
 
-def request(book, chapter, verse):
-    if verse:
-        url = url_base + book + chapter + ":" + verse + "?translation=almeida"
+def menu():
+    while True:
+        print(f'Actual Translation: {translation}\n(1) Get new reference\n(2) Next Chapter\n(3) Previous Chapter\n(4) Change Translation')
+        option = int(input(":"))
+        if option == 1: set_reference() 
+        elif option == 2: next_chapter()
+        elif option == 3: previous_chapter()
+        elif option == 4: set_translation()
+        else: break
+
+def set_reference():
+    global actual_reference
+    global translation
+    pattern = r"^[1-3]?\s?[A-Za-zÀ-ÖØ-öø-ÿ]+(?:\s[A-Za-zÀ-ÖØ-öø-ÿ]+)*\s\d+:\d+$"
+    reference = input("Type the biblical reference (ex: Genesis 1:1): ")
+    
+    if re.match(pattern, reference):    
+        url = f'{url_api}/{reference}?translation={translation}'
+        response = requests.get(url, verify=False)
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+
+            actual_reference = reference
+            print(reference)
+        else:
+            print("Invalid reference")
     else:
-        url = url_base + book + chapter + "?translation=almeida"
-        
+        print("Invalid format.")
+        set_reference()
+
+def set_translation():
+    global translation
+    url = f'{url_api}/data'
     response = requests.get(url, verify=False)
 
     if response.status_code == 200:
-        data = response.json()
-        formatted_verses = [f"{verse['verse']}.{verse['text']}" for verse in data["verses"]]
-        for verse in formatted_verses:
-            print(verse) 
+        data = response.json().get("translations", [])
+        print("Translactions: ")
+        print("\n".join(f"{t['identifier']} - {t['name']}" for t in data))
+        translations = [t['identifier'] for t in data]
+
+        translation = input("Select the desired translaction: ")
+        
+        while translation not in translations:
+            print("Invalid Translation")
+            translation = input("Select the desired translaction: ")
+
+def next_chapter():
+    pass
+
+def previous_chapter():
+    pass
 
 def main():
-    book = input("Book: ")
-    chapter = input("Chapter: ")
-    verse = input("Verse: ")
-
-    request(book, chapter, verse)
+    set_translation()
+    menu()
 
 if __name__ == "__main__":
     main()
